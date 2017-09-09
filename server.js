0e8dfab6390640eed347f3fa4bedd3152272f718
Truong Hoai Nhan <nhan.truong@alta.com.vn>
2017-09-09 15:25:04 +0700

    edit

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
